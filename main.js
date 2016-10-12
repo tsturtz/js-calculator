0037c8b289f3a87bd0ce7c7a9863171e1961dbbf
Taylor Sturtz <taylorsturtz.com>
2016-10-12 13:09:01 -0700

    calculator version0.5 - Taylor Sturtz

--- main.js
+++ main.js
@@ -1,36 +1,31 @@
-$(document).ready(function(){
-    apply_click_handlers();
-});
-
-//click handlers
-function apply_click_handlers(){
-    console.log('click handlers called');
-    $('.btn_c').click(display_function);
-    $('.btn_ce').click(display_function);
-    $('.btn_0').click(display_function);
-    $('.btn_1').click(display_function);
-    $('.btn_2').click(display_function);
-    $('.btn_3').click(display_function);
-    $('.btn_4').click(display_function);
-    $('.btn_5').click(display_function);
-    $('.btn_6').click(display_function);
-    $('.btn_7').click(display_function);
-    $('.btn_8').click(display_function);
-    $('.btn_9').click(display_function);
-    $('.btn_dot').click(display_function);
-    $('.btn_equals').click(display_function);
-    $('.btn_plus').click(display_function);
-    $('.btn_minus').click(display_function);
-    $('.btn_multiply').click(display_function);
-    $('.btn_divide').click(display_function);
-}
-
 var my_calculator = new calculator(display_function);
 
-/*type will be a string equal to one of the following:
-* itemAdded, calculated, error*/
+/*type will be a string equal to one of the following: itemAdded, calculated, error*/
 function display_function(type,value,item){
-    var val = '';
-    $('.calc-display').text(value);
+    switch (value){
+        case undefined:
+            $('.calc-display').text('');
+            break;
+        default:
+            $('.calc-display').text(value);
+            break;
+    }
     console.log('display_function called');
 }
+
+$(document).ready(function(){
+    $('.btn-lg').click(function(){
+        var val = $(this).text();
+        switch (val) {
+            case 'CE':
+                my_calculator.allClear();
+                break;
+            case 'C':
+                my_calculator.clear();
+                break;
+            default:
+                my_calculator.addItem($(this).text());
+                break;
+        }
+    });
+});
