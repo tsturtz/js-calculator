72da1c4d864135a33e6bb833bfc80e012069735c
Taylor Sturtz <taylorsturtz.com>
2016-10-16 14:46:11 -0700

    got CE to work and multiple decimals and operators check

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
     // APPLY CLICK HANDLERS
 
-    $('.operand').click(operand_click);
-    $('.operator').click(operator_click);
-    $('.equals').click(equals_click);
-    $('.clear').click(clear_click);
-    $('.clear_entry').click(clear_entry_click);
+    apply_click_handlers();
+
 });
 
 // GLOBAL VARIABLES
@@ -14,47 +11,102 @@ var input_array = ['']; // Create empty array that will store input
 var input_index = 0; // Set input index position to 0
 var display; // Display current input
 
+// CLICK HANDLERS
+
+function apply_click_handlers() {
+    $('.operand').click(operand_click);
+    $('.operator').click(operator_click);
+    $('.decimal').click(decimal_click);
+    $('.equals').click(equals_click);
+    $('.clear').click(clear_click);
+    $('.clear_entry').click(clear_entry_click);
+}
+
 // BUTTON CLICK FUNCTIONS
 
 function operand_click() {
-    input_array[input_index] += $(this).text();
-    console.log('input array: ', input_array);
-    display_value();
+    if(input_array.length === 1){
+        // Adds text of current click to the current index position string
+        input_array[input_index] += $(this).text();
+        console.log('input array: ', input_array);
+        display_value();
+    }else{
+        // Increments out of operator to next index position to receive next operand input
+        input_index++;
+        // Prepares next input to be a string (?) Confused on why without this I receive the following example in the next index position: example: 'undefined3'
+        input_array[input_index] = '';
+        // Adds text of current click to the current index position string
+        input_array[input_index] += $(this).text();
+        console.log('input array: ', input_array);
+        display_value();
+    }
 }
 
 function operator_click() {
-    input_index++;
-    input_array[input_index] = $(this).text();
-    console.log('input array: ', input_array);
-    input_index++;
-    display_value();
-    input_array[input_index] = '';
+    // Checks for and prevents multiple operator inputs
+    if(input_array[input_array.length-1].includes('+') || input_array[input_array.length-1].includes('-') || input_array[input_array.length-1].includes('x') || input_array[input_array.length-1].includes('/') ){
+        console.log('multiple operators not allowed');
+    }
+    else{
+        // Increments from last index position to receive new operator input
+        input_index++;
+        // Adds operator string to current index position
+        input_array[input_index] = $(this).text();
+        console.log('input array: ', input_array);
+        // Increments out of operator to next index position to receive next operand input
+        //input_index++;
+        display_value();
+        // Prepares next input to be a string (?) Confused on why without this I receive the following example in the next index position: example: 'undefined3'
+        //input_array[input_index] = '';
+    }
+}
+
+function decimal_click(){
+    // Checks for and prevents multiple decimal inputs
+    if(input_array[input_array.length-1].includes('.')){
+        console.log('multiple decimals not allowed');
+    }
+    // Adds single decimal to string, same as an operand
+    else{
+        input_array[input_index] += $(this).text();
+        console.log('input array: ', input_array);
+        display_value();
+    }
 }
 
 function equals_click() {
     console.log(input_array);
+    // Clears display
     $('.calc-display').text('');
+    // Calls function to do math and adds result to display
     $('.calc-display').append(do_math(input_array[0], input_array[2]));
 }
 
 function clear_click() {
+    // Resets array to starting position
     input_array = [''];
+    // Resets index position to starting position
     input_index = 0;
+    // Displays 0 on calculator display
     $('.calc-display').text('0');
     console.log(input_array);
 }
 
 function clear_entry_click() {
+    // Removes last index entry from array
     input_array.pop();
-    input_array = [''];
-    console.log(input_array);
+    // Decrements index position to be ready for next input
+    input_index--;
+    console.log(input_index);
     display_value();
 }
 
 // ADD TO DISPLAY
 
 function display_value() {
+    // Joins together array indexes into a string
     display = input_array.join('');
+    // Displays joined string on calculator display
     $('.calc-display').text(display);
 }
 
@@ -65,13 +117,13 @@ function do_math(num1, num2) {
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
     var operator = input_array[1];
-    if (operator == '+') {
+    if (operator === '+') {
         result = num1 + num2;
-    } else if (operator == '-') {
+    } else if (operator === '-') {
         result = num1 - num2;
-    } else if (operator == 'x') {
+    } else if (operator === 'x') {
         result = num1 * num2;
-    } else if (operator == '/') {
+    } else if (operator === '/') {
         result = num1 / num2;
     }
     console.log(result);
